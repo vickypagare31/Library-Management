27e6620821a3921ca0aed0cc1fbf11f9d825e58b
Vicky Pagare <vickypagare31@gmail.com>
2024-09-08 20:13:02 +0530

    Added Borrwer class

--- Borrower.java
+++ Borrower.java
@@ -1,4 +1,30 @@
 package com.library_Management.Library.Management.System.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "borrowers")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Borrower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    private List<Book> borrowedBooks=new ArrayList<>();
+
 }
